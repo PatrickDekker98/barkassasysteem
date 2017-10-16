0241c58f048d302a759ee9b99787c73592a24f9e
Bart <bart.mommers@student.hu.nl>
2017-10-16 10:25:16 +0200

    Productmanipulation - version 2.0

--- code/productManipulation.py
+++ code/productManipulation.py
@@ -24,34 +24,12 @@ def alterPrice(name, newprice):
     cursor.execute(''' UPDATE price SET value = ? WHERE productId = ?''', (newprice, id))
     db.commit()
 
-#Tijdelijke database in geheugen aanmaken
-db = sqlite3.connect(':memory:')
+db = sqlite3.connect('..\\barkassasysteem.db')
 cursor = db.cursor()
 
-#Database tabellen maken
-cursor.execute('''CREATE TABLE product(productId INTEGER PRIMARY KEY, name TEXT, datetimeStart DATETIME, datetimeEnd DATETIME)''')
-cursor.execute('''CREATE TABLE price(productId INTEGER , datetimeStart DATETIME, datetimeEnd DATETIME, value DECIMAL, FOREIGN KEY(productId) REFERENCES product(productId), PRIMARY KEY (productId, datetimeStart))''')
-db.commit()
-
-#Dummy data invoeren
-addProduct(name='Bier', startdate='04-10-2017', price=1.20)
-addProduct(name='Cola', startdate='05-10-2017', price=1.00)
-addProduct(name='Fanta', startdate='04-10-2017', price=1.20)
-addProduct(name='7-up', startdate='05-10-2017', price=1.00)
-deactivateProduct('Bier', '20-10-2017')
-alterPrice('Cola', 1.95)
-
-#cursor.execute('''SELECT * FROM Product''')
-#for row in cursor:
-#    print(row)
-
-#cursor.execute('''SELECT * FROM Prijs''')
-#for row in cursor:
-#    print(row)
-
 cursor.execute('''SELECT name, value, product.datetimeStart, product.datetimeEnd FROM Product INNER JOIN price ON Product.productId = price.productId''')
-print('{:15} {:10} {:15} {:10}'.format('Productnaam', 'Prijs', 'Start datum', 'Eind datum'))
+print('{:30} {:10} {:15} {:10}'.format('Productnaam', 'Prijs', 'Start datum', 'Eind datum'))
 for row in cursor:
-    print('{:15} {:<10.2f} {:15} {:10}'.format(row[0], row[1], row[2], str(row[3])))
+    print('{:30} {:<10.2f} {:<15} {:10}'.format(row[0], row[1], row[2], str(row[3])))
 
 db.close()
