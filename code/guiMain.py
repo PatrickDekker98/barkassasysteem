833db33d2834bdba24efbceb3812722a6fa20979
Bart <bart.mommers@student.hu.nl>
2017-10-25 16:56:28 +0200

    Added a bit more structure... Work in progress

--- code/guiMain.py
+++ code/guiMain.py
@@ -4,21 +4,21 @@ import tkinter, guiNewTransaction, guiCategories
 class topLevelWindow:
     def __init__(self, master):
         self.master = master
-        #self.master.wm_attributes('-fullscreen', 'true')
+        self.master.wm_attributes('-fullscreen', 'true')
 
-        menuFrame = tkinter.Frame(self.master)
+        menuFrame = tkinter.Frame(self.master, bg='lightblue')
         contentFrame = tkinter.Frame(self.master)
-        brandingFrame = tkinter.Frame(self.master)
+        #brandingFrame = tkinter.Frame(self.master, bg='lightblue')
         footerFrame = tkinter.Frame(self.master)
 
-        self.master.columnconfigure(0, weight=2)
-        self.master.columnconfigure(1, weight=86)
-        self.master.columnconfigure(2, weight=10)
-        self.master.columnconfigure(3, weight=2)
-        self.master.rowconfigure(0, weight=10)
+        self.master.columnconfigure(0, minsize=50, weight=2)
+        self.master.columnconfigure(1, weight=9)
+        #self.master.columnconfigure(2, weight=1)
+        self.master.columnconfigure(3, minsize=50, weight=2)
+        self.master.rowconfigure(0, minsize=50, weight=10)
         self.master.rowconfigure(1, weight=85)
-        self.master.rowconfigure(2, weight=5)
-#
+        self.master.rowconfigure(2, minsize=50, weight=5)
+
         menuFrame.grid(column=1,row=0,columnspan=2)
         contentFrame.grid(column=1,row=1, sticky='news')
         #brandingFrame.grid(column=2,row=1, sticky='news')
@@ -34,10 +34,14 @@ class topLevelWindow:
 
 class buildMenu:
     def __init__(self, master):
-        tkinter.Button(master, text='Transaction', command=lambda: my_gui.content.callTransaction()).grid(column=0,row=0, sticky='news')
-        tkinter.Button(master, text='Klanten', command=lambda: my_gui.content.callCustomers()).grid(column=1,row=0, sticky='news')
-        tkinter.Button(master, text='Producten', command=lambda: my_gui.content.callProducts()).grid(column=2,row=0, sticky='news')
-        tkinter.Button(master, text='Categorieën', command=lambda: my_gui.content.callCategories()).grid(column=3,row=0, sticky='news')
+        master.rowconfigure(0, weight=1)
+        for c in range(4):
+            master.columnconfigure(c, weight=1)
+        font = ('Arial Black', 15)
+        tkinter.Button(master, width=200, height=3, font=font, bg='lightblue', text='Transaction', command=lambda: my_gui.content.callTransaction()).grid(column=0,row=0, sticky='news')
+        tkinter.Button(master, width=200, height=3, font=font, bg='lightblue',text='Klanten', command=lambda: my_gui.content.callCustomers()).grid(column=1,row=0, sticky='news')
+        tkinter.Button(master, width=200, height=3, font=font, bg='lightblue',text='Producten', command=lambda: my_gui.content.callProducts()).grid(column=2,row=0, sticky='news')
+        tkinter.Button(master, width=200, height=3, font=font, bg='lightblue',text='Categorieën', command=lambda: my_gui.content.callCategories()).grid(column=3,row=0, sticky='news')
 
 
 class buildContent(topLevelWindow):
