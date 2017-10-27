4c12513961279720931faa078b2c2c4e8c0f9031
EchteBoter <patrick.p.dekker@student.hu.nl>
2017-10-27 22:17:18 +0200

    very nice commit customer gui shows more info, comments were added too

--- code/customerFuncs.py
+++ code/customerFuncs.py
@@ -1,13 +1,15 @@
 from main import *
 
+#deze functie geeft alle klanten met alle data terug
 def returnAllCustomers():
-    customersData = cursor.execute('SELECT firstName, insertion, lastName, customerId FROM customer')
+    customersData = cursor.execute('SELECT firstName, insertion, lastName, customerId, balance, telegramId FROM customer')
     customers    = dict()
     for customer in customersData:
-        customers[customer[3]] = [customer[0], customer[2], customer[1]]
+        customers[customer[3]] = [customer[0], customer[2], customer[1], customer[4], customer[5]]
     
     return customers
 
+#deze functie geef aleen de naam van een klant terug
 def returnOneCustomer(customerId):
     customerData = cursor.execute('SELECT firstName, insertion, lastName FROM customer WHERE customerId = ?', customerId)
     for customer in customerData:
@@ -15,6 +17,7 @@ def returnOneCustomer(customerId):
 
     return customerReturn
 
+#deze functie geeft alle telegram ids terug
 def returnAllTelegramIds():
     telegramIds = cursor.execute('SELECT telegramId FROM customer WHERE telegramId IS NOT NULL')
     returnIds = list()
@@ -22,20 +25,29 @@ def returnAllTelegramIds():
         returnIds.append(Ids[0])
     return returnIds
 
+#nu kan je een nieuw telegram id koppelen met een klant
+def addTelegramId(customerId, telegramId):
+    cursor.execute('UPDATE customer SET telegramId = ? WHERE customerId = ?', (telegramId, customerId))
+    conn.commit()
+
+#dit verwijderd een klant uit de database
 def unsetDb(customerId):
     cursor.execute('DELETE FROM customer WHERE customerId = ?', (str(customerId)))
     conn.commit()
 
+#alleen als confirm als true word meegegeven kan de klant worden verwijderd
 def deactivateCustomer(customerId, confirm = False):
     if confirm:
         unsetDb(customerId)
 
+#werk een klant bij, als inserion een lege string is word het none
 def insertDb(customerId, firstName, insertion, lastName):
     if insertion == '':
         insertion = None
     cursor.execute('UPDATE customer SET firstName = ?, insertion = ?, lastName= ? WHERE customerID = ?', (firstName, insertion, lastName, customerId))
     conn.commit()
 
+#alleen als confirm als true word meegegeven kan een klant worden bewerkt
 def editCustomer(customerId, firstName, insertion, lastName, confirm = False):
     if confirm:
        insertDb(customerId, firstName, insertion, lastName)
--- code/guiCustomers.py
+++ code/guiCustomers.py
@@ -2,18 +2,20 @@ from main import *
 from customerFuncs import *
 import tkinter
 
+#dit maakt een customer frame
 class customers:
     def __init__(self, master):
         self.master = master
         
         customerShowFrame = tkinter.Frame(self.master)
-        customerShowFrame.grid(column = 4, row = 1, columnspan = 2, padx = 20, pady = 20)
+        customerShowFrame.grid(column = 4, row = 0, columnspan = 2, padx = 20, pady = 20)
         self.customerShow = customerTable(customerShowFrame)
 
         customerAddFrame = tkinter.Frame(self.master)
-        customerAddFrame.grid(column = 6, row = 1, columnspan = 2, padx = 20, pady = 20)
+        customerAddFrame.grid(column = 6, row = 0, columnspan = 2, padx = 20, pady = 20)
         self.addFrame = addCustomerFrame(customerAddFrame)
 
+#dit maakt een entry veld voor nieuwe klant
 class addCustomerFrame:
     def __init__(self, master):
         tkinter.Label(master,
@@ -53,6 +55,8 @@ class addCustomerFrame:
                 bg = "green",
                 command = lambda: self.confirmAddition()).grid(row = 5, column = 0, columnspan = 2)
 
+    #dit controleerd of de voornaam of achternaam wel of niet leeg is
+    #het geeft ook een popup als het wel of niet gelukt is
     def confirmAddition(self):
         firstName = self.firstNameEntry.get()
         insertion = self.insertionEntry.get()
@@ -77,20 +81,28 @@ class addCustomerFrame:
                     font = ('Arial', 15),
                     height = 2,).pack()
 
+#dit maakt een tabel voor de klanten plus drie knoppen om ze te bewerken
 class customerTable:
     def __init__(self, master):
+        idColumn       = 1
+        nameColumn     = 2
+        moneyColumn    = 3
+        telegramColumn = 4
         customerDict = returnAllCustomers()
         tkinter.Label(master, 
                 text = 'Bewerk en/of verwijder klanten:', 
                 font = ('Arial', 15), 
                 height = 2).grid(column = 1, row = 0, columnspan = 3)
 
-        tkinter.Label(master, text = "Id").grid(column = 1, row = 1, sticky = "W")
-        tkinter.Label(master, text = "Naam").grid(column = 2, row = 1, sticky = "W")
+        tkinter.Label(master, text = "Id").grid(column = idColumn, row = 1, sticky = "W")
+        tkinter.Label(master, text = "Naam").grid(column = nameColumn, row = 1, sticky = "W")
+        tkinter.Label(master, text = "geld").grid(column = moneyColumn, row = 1, sticky = "W")
+        tkinter.Label(master, text = "telegram").grid(column = telegramColumn, row = 1, sticky = "W")
 
         count = 2
         for customerKey in customerDict:
             cmdDelete = lambda customerList = customerDict[customerKey], customerKey = customerKey: self.confirmUnset(master, customerList, customerKey)
+            cmdTelegram = lambda customerList = customerDict[customerKey], customerKey = customerKey: self.addTelegramId(master, customerList, customerKey)
             cmdEdit = lambda customerList = customerDict[customerKey], customerKey = customerKey: self.confirmEdit(master, customerList, customerKey)
             customerList = customerDict[customerKey]
             
@@ -99,23 +111,113 @@ class customerTable:
             else :
                 text = customerList[0] + ' ' + customerList[1]
 
-            tkinter.Label(master, text = str(customerKey)).grid(column = 1, row = count, sticky = "W")
-            tkinter.Label(master, text = text).grid(column = 2, row = count, sticky = "W")
+            if customerList[4] != None :
+                telegramText = str(customerList[4])
+            else :
+                telegramText = 'geen'
+
+            tkinter.Label(master, 
+                    text = str(customerKey)).grid(column = idColumn, row = count, sticky = "W")
+            tkinter.Label(master, 
+                    text = text).grid(column = nameColumn, row = count, sticky = "W")
+            tkinter.Label(master,
+                    text = str(customerList[3])).grid(column = moneyColumn, row = count, sticky = "W")
+            tkinter.Label(master,
+                    text = telegramText).grid(column = telegramColumn, row = count, sticky = "W")
+
             tkinter.Button(master, 
                     text = "bewerk klant", 
                     height = 1, 
                     width = 15, 
                     bg = 'lightgreen', 
-                    command = cmdEdit).grid(column = 3, row = count)
+                    command = cmdEdit).grid(column = 5, row = count)
+            tkinter.Button(master, 
+                    text = "telegram",
+                    height = 1, 
+                    width = 15,
+                    bg = 'blue',
+                    fg = 'white',
+                    command = cmdTelegram).grid(column = 6, row = count)
             tkinter.Button(master, 
                     text = "verwijder klant", 
                     height = 1, 
                     width = 15, 
                     bg = 'red', 
-                    command = cmdDelete).grid(column = 4, row = count)
+                    command = cmdDelete).grid(column = 7, row = count)
             
             count += 1
 
+    #maakt een popup die vraagt om een telegramid die niet leeg is en 9 karakters lang is 
+    def addTelegramId(self, master, customerList, customerKey):
+        popup = tkinter.Toplevel()
+        popup.wm_title("Confirmation")
+        popup.geometry('500x100')
+        popup.resizable(width=False, height=False)
+
+        if customerList[2] != None:
+            text = "verander of voeg het telegramID van klant " + customerList[0] + ' ' + customerList[2] + ' ' +customerList[1] + ' met id ' + str(customerKey) + ' toe'
+        else :
+            text = "verander of voeg het telegramId van klant " + customerList[0] + ' ' +customerList[1] + ' met id ' + str(customerKey) + ' toe'
+        label = tkinter.Label(popup, text = text).grid(row = 1, columnspan = 6)
+        
+        
+        telegramIdLabel = tkinter.Label(popup,
+                text = "telegramID:",
+                height = 1,
+                width = 12).grid(row = 2, column = 0)
+        self.telegramIdEntry = tkinter.Entry(popup, width = 10)
+        self.telegramIdEntry.grid(row = 2, column = 1)
+
+        cancelButton = tkinter.Button(popup, 
+                text = "annuleer", 
+                height = 1, 
+                width = 10, 
+                command = lambda: popup.destroy()).grid(row = 2, column = 3)
+
+        confirmButton = tkinter.Button(popup, 
+                text = "ok", 
+                height = 1, 
+                width = 10, 
+                command = lambda: self.addTelAndDestroy(popup, customerKey)).grid(row = 2, column = 4)
+
+    #callt naar de telegram id add functie en sluit de popup
+    def addTelAndDestroy(self, popup, customerKey):
+        self.telegramIdAdd(customerKey)
+        popup.destroy()
+
+    #dit controleerd de input van de entry velden en start de functie addTelegramId die met de database communiseerd 
+    def telegramIdAdd(self, customerKey):
+        telegramId = self.telegramIdEntry.get()
+        if telegramId == '':
+            popup = tkinter.Toplevel()
+            popup.wm_title('Error')
+            popup.geometry('500x100')
+            popup.resizable(width=False, height=False)
+            tkinter.Label(popup,
+                    text = "een telegram id mag niet leeg zijn",
+                    font = ('Arial', 15),
+                    height = 2,).pack()
+        elif len(telegramId) != 9:
+            popup = tkinter.Toplevel()
+            popup.wm_title('Error')
+            popup.geometry('500x100')
+            popup.resizable(width=False, height=False)
+            tkinter.Label(popup,
+                    text = "een telegram id moet 9 cijfers zijn",
+                    font = ('Arial', 15),
+                    height = 2,).pack()
+        else :
+            addTelegramId(customerKey, telegramId)
+            popup = tkinter.Toplevel()
+            popup.wm_title('succes')
+            popup.geometry('500x100')
+            popup.resizable(width=False, height=False)
+            tkinter.Label(popup,
+                    text = "Gelukt",
+                    font = ('Arial', 15),
+                    height = 2,).pack()
+    
+    #deze functie vraagt om de nieuwe naam van de klant, en om een confirmatie  
     def confirmEdit(self, master, customerList, customerKey):
         popup = tkinter.Toplevel()
         popup.wm_title("Confirmation")
@@ -162,10 +264,12 @@ class customerTable:
                 width = 10, 
                 command = lambda: self.editAndDestroy(popup, customerKey)).grid(row = 3, column = 4)
 
+    #deze functie roept twee functies 
     def editAndDestroy(self, popup, customerKey):
         self.customerEdit(customerKey)
         popup.destroy()
 
+    #customerEdit checkt of firstname en lastName niet leeg zijn, en geeft een schermpje terug of het wel of niet gelukt is
     def customerEdit(self, customerKey):
         firstName = self.firstNameEntry.get()
         insertion = self.insertionEntry.get()
@@ -190,7 +294,7 @@ class customerTable:
                     font = ('Arial', 15),
                     height = 2,).pack()
         
-
+    #deze functie vraagt of je zeker weet of je de klant wil verwijderen
     def confirmUnset(self, master, customerList, customerKey):
         popup = tkinter.Toplevel()
         popup.wm_title("Confirmation")
@@ -212,7 +316,7 @@ class customerTable:
                 height = 1, 
                 width = 10, 
                 command = lambda: self.deactivateAndDestroy(popup, customerKey)).grid(row = 3, column = 0)
-
+    #deze functie roept twee functies aan
     def deactivateAndDestroy(self, popup, customerKey):
         deactivateCustomer(customerKey, True)
         popup.destroy()
