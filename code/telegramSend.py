8f83b8d3165798b4d6d0445256af8c1ed742efa5
Bart <bart.mommers@student.hu.nl>
2017-10-27 15:40:19 +0200

    fixed broken variable

--- code/telegramSend.py
+++ code/telegramSend.py
@@ -4,7 +4,7 @@ import telepot
 def sendToAll(msg) :
     token = '466467969:AAHZ253Zam0jPyJaqTxQZ6baCbWnvatsxt4'
     bot = telepot.Bot(token)
-    telegramIs = returnAllTelegramIds()
+    telegramIds = returnAllTelegramIds()
     for telegramId in telegramIds:
         bot.sendMessage(telegramId, str(msg))
 
