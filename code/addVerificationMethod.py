5dc36b39738879322733911b6e119e35f037d63a
Bart <bart.mommers@student.hu.nl>
2017-10-28 00:02:09 +0200

    Added docStrings and commentary

--- code/addVerificationMethod.py
+++ code/addVerificationMethod.py
@@ -2,6 +2,7 @@ from main import *
 
 
 def addVerificationMethod(customerId, verificationTypeId, reference):
+    'Adds a verificationmethod for a user to the database, using CustomerId, verificationtypeId, and the reference'
     try:
         cursor.execute('INSERT INTO verificationMethod (customerId, verificationTypeId, reference) VALUES (?,?,?)', (customerId, verificationTypeId, reference))
         conn.commit()
@@ -10,10 +11,3 @@ def addVerificationMethod(customerId, verificationTypeId, reference):
     except:
         raise ValueError("Verification method could not be added to the database. Please try again.")
 
-
-print(addVerificationMethod(1, 1, "8248"))
-print(addVerificationMethod(18, 1, 2938))
-
-data = cursor.execute("select * from verificationMethod")
-for row in data:
-    print(row)
--- code/customerFuncs.py
+++ code/customerFuncs.py
@@ -1,59 +1,67 @@
 from main import *
 
-#deze functie geeft alle klanten met alle data terug
+
 def returnAllCustomers():
+    'Fetches and returns all customers'
     customersData = cursor.execute('SELECT firstName, insertion, lastName, customerId, balance, telegramId FROM customer')
     customers    = dict()
     for customer in customersData:
         customers[customer[3]] = [customer[0], customer[2], customer[1], customer[4], customer[5]]
-    
     return customers
 
-#deze functie geef aleen de naam van een klant terug
+
 def returnOneCustomer(customerId):
+    'takes a customerId, and returns the customers name'
     customerData = cursor.execute('SELECT firstName, insertion, lastName FROM customer WHERE customerId = ?', customerId)
     for customer in customerData:
         customerReturn = [customer[0], customer[2], customer[1]]
 
     return customerReturn
 
-#deze functie geeft alle telegram ids terug
+
 def returnAllTelegramIds():
+    'Returns all documented telegram IDs'
     telegramIds = cursor.execute('SELECT telegramId FROM customer WHERE telegramId IS NOT NULL')
     returnIds = list()
     for Ids in telegramIds:
         returnIds.append(Ids[0])
     return returnIds
 
-#nu kan je een nieuw telegram id koppelen met een klant
+
 def addTelegramId(customerId, telegramId):
+    'Add a customers Telegram ID to the database'
     cursor.execute('UPDATE customer SET telegramId = ? WHERE customerId = ?', (telegramId, customerId))
     conn.commit()
 
-#dit verwijderd een klant uit de database
+
 def unsetDb(customerId):
+    'Deletes a customer from the database'
     cursor.execute('DELETE FROM customer WHERE customerId = ?', (str(customerId)))
     conn.commit()
 
-#alleen als confirm als true word meegegeven kan de klant worden verwijderd
+
 def deactivateCustomer(customerId, confirm = False):
+    'If confirmation is given, the customer will be deleted from the database. Takes a customerId, and optionally the confirmation in Boolian'
     if confirm:
         unsetDb(customerId)
 
-#werk een klant bij, als inserion een lege string is word het none
+
 def insertDb(customerId, firstName, insertion, lastName):
+    'Edits a customer in the database'
     if insertion == '':
         insertion = None
     cursor.execute('UPDATE customer SET firstName = ?, insertion = ?, lastName= ? WHERE customerID = ?', (firstName, insertion, lastName, customerId))
     conn.commit()
 
-#alleen als confirm als true word meegegeven kan een klant worden bewerkt
+
 def editCustomer(customerId, firstName, insertion, lastName, confirm = False):
+    'Checks for confirmation before customer information in database is altered'
     if confirm:
        insertDb(customerId, firstName, insertion, lastName)
 
-#addCustomer adds customer, if insertion is an empty string it replaces it with a NULL value, balance is standard 0 when creating a new customer
+
 def addCustomer(firstName, insertion, lastName):
+    'adds customer, if insertion is an empty string it replaces it with a NULL value, balance is standard 0 when creating a new customer'
     balance = 0.0
     if insertion == '':
         insertion = None
