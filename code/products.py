a343fd219fc2136a643f9a7fe241e1d1e4b35a42
Bart <bart.mommers@student.hu.nl>
2017-10-27 15:40:52 +0200

    Added functions for productmanipulation in database

--- code/products.py
+++ code/products.py
@@ -1,47 +1,41 @@
 from main import *
 
 def lookupProductName(id):
+    print(id)
     cursor.execute('''SELECT name FROM Product WHERE productId = ?''', (id,))
-    productname = cursor.fetchone()[0]
-    return productname
+    return cursor.fetchone()[0]
 
 def lookupProductId(productname):
     cursor.execute('''SELECT productId FROM Product WHERE name = ?''', [productname,])
     return cursor.fetchone()[0]
 
 def calculateProductPrice(productid):
-    epochTime = str(datetime.date.today())
-    cursor.execute('''SELECT EXISTS(SELECT * FROM discount d WHERE d.productid = ? AND d.datetimeStart < ? AND (d.datetimeEnd > ? OR d.datetimeEnd IS NULL))''', (productid, epochTime, epochTime))
+    cursor.execute('''SELECT EXISTS(SELECT * FROM discount WHERE productid = ? AND datetimeStart <= datetime('now', 'localtime') AND (datetimeEnd >= datetime('now', 'localtime') OR datetimeEnd IS NULL))''', (productid,))
     if cursor.fetchone()[0] == 1:
-        cursor.execute('''SELECT d.productId, p.value, d.percentage FROM discount d INNER JOIN price p ON p.productId = d.productId WHERE d.productid = ? AND d.datetimeStart < ? AND (d.datetimeEnd > ? OR d.datetimeEnd IS NULL)''',(productid, epochTime, epochTime))
+        cursor.execute('''SELECT d.productId, p.value, d.percentage FROM discount d INNER JOIN price p ON p.productId = d.productId WHERE d.productid = ? AND d.datetimeStart <= datetime('now', 'localtime') AND (d.datetimeEnd >= datetime('now', 'localtime') OR d.datetimeEnd IS NULL)''',(productid, ))
         productDiscount = cursor.fetchone()
         productPrice = productDiscount[1] * productDiscount[2]
         return eval('{:.2f}'.format(productPrice))
     else:
-        try:
-            cursor.execute('''SELECT p.value FROM price p WHERE p.productid = ? AND p.datetimeStart < ? AND (p.datetimeEnd > ? OR p.datetimeEnd IS NULL)''',(productid, epochTime, epochTime))
-            productPrice = cursor.fetchone()
-            return productPrice[0]
-        except:
-            messageBox('Geen product geselecteerd','U heeft geen product geselecteerd. Selecteer een product voordat u deze toe wilt voegen aan te transactie.','error')
-            return
+        cursor.execute('''SELECT value FROM price WHERE productId = ? AND datetimeStart <= datetime('now', 'localtime') AND (datetimeEnd >= datetime('now', 'localtime') OR datetimeEnd IS NULL)''',(productid, ))
+        productPrice = cursor.fetchone()
+        return productPrice[0]
 
 def fetchProducts():
     cursor.execute("SELECT pro.name, pri.value, pri.datetimeStart, pri.datetimeEnd FROM product pro INNER JOIN price pri ON pro.productId = pri.productId  WHERE pri.datetimeStart <= datetime('now', 'localtime') AND (pri.datetimeEnd ISNULL OR pri.datetimeEnd > datetime('now', 'localtime'))")
     return cursor.fetchall()
 
 def fetchProductsPerCategory(categories):
-    epochTime = str(datetime.date.today())
     products = dict()
     for category in categories:
-        cursor.execute('''SELECT productId, name FROM product WHERE datetimeStart < ? AND (datetimeEnd > ? OR datetimeEnd IS NULL) AND categoryId IS ?''',(epochTime, epochTime, category[0]))
+        cursor.execute('''SELECT productId, name FROM product WHERE datetimeStart <= datetime('now', 'localtime') AND (datetimeEnd >= datetime('now', 'localtime') OR datetimeEnd IS NULL) AND categoryId IS ?''',[category[0]])
         products[category] = cursor.fetchall()
-    cursor.execute('''SELECT productId, name FROM product WHERE datetimeStart < ? AND (datetimeEnd > ? OR datetimeEnd IS NULL) AND categoryId IS NULL''',(epochTime, epochTime))
     return products
 
-def addProduct(productName, productDatetimeStart, priceValue, priceDatetimeStart, productDatetimeEnd=None, priceDatetimeEnd=None):
+def addProduct(productName, productDatetimeStart, priceValue, priceDatetimeStart, categoryId, productDatetimeEnd=None, priceDatetimeEnd=None):
     'Takes a name, startdate, price and optionally an enddate. This function creates the new product and prices it.'
-    cursor.execute(''' INSERT INTO product(name,datetimeStart) VALUES(?,?)''', [productName, productDatetimeStart])
+    print(categoryId)
+    cursor.execute(''' INSERT INTO product(name,datetimeStart, categoryId) VALUES(?,?,?)''', [productName, productDatetimeStart, categoryId])
     cursor.execute('''SELECT productId FROM product WHERE name = ?''', [productName,])
     id = cursor.lastrowid
     cursor.execute(''' INSERT INTO price( productId, datetimeStart, value) VALUES(?,?,?)''', [id, priceDatetimeStart,priceValue])
@@ -60,5 +54,14 @@ def setNewProductPrice(productId, price, newPriceDatetimeStart, newPriceDatetime
     if newPriceDatetimeEnd == '':
         newPriceDatetimeEnd = None
     cursor.execute('''UPDATE price SET datetimeEnd = ? WHERE productId = ?''', [newPriceDatetimeStart, productId])
-    cursor.execute('''INSERT INTO price(productId,value,datetimeStart,datetimeEnd)''', [productId, price, newPriceDatetimeStart,newPriceDatetimeEnd])
+    cursor.execute('''INSERT INTO price(productId,value,datetimeStart,datetimeEnd) VALUES(?,?,?,?)''', [productId, price, newPriceDatetimeStart,newPriceDatetimeEnd])
+    conn.commit()
 
+def lookupProductCategory(productId):
+    cursor.execute('''SELECT categoryId FROM product WHERE productId = ?''', [productId])
+    return cursor.fetchone()[0]
+
+def setNewProductCategory(productId, categoryId):
+    cursor.execute('''UPDATE product SET categoryId = ? WHERE productId = ?''', [categoryId, productId])
+    conn.commit()
+    return
